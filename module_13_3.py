038d87c561c49a96c1adfbb9da58a549ab0bab28
AndreyWarnin <archyrysss@yahoo.com>
2025-01-26 13:11:06 +0300

    Add files via upload

--- module_13_3.py
+++ module_13_3.py
@@ -10,10 +10,10 @@ dp = aiogram.Dispatcher(bot, storage=MemoryStorage())
 
 @dp.message_handler(commands=['start'])
 async def start(message):
-    task = create_task(message.answer('Это отвечает БОТ на команду Start'))
+    task = create_task(message.answer('Привет! Я бот помогающий твоему здоровью.'))
 
 @dp.message_handler()
 async def all_messages(message):
-    task = create_task(message.answer('Это отвечает БОТ на любое сообщение'))
+    task = create_task(message.answer('Введите команду /start, чтобы начать общение.'))
 
 aiogram.executor.start_polling(dp, skip_updates=True)
