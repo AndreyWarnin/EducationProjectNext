6b3268f3b715c3aa23700f100874796faabf69f7
AndreyWarnin <archyrysss@yahoo.com>
2025-01-26 13:16:20 +0300

    Add files via upload

---
+++ module_13_5.py
@@ -0,0 +1,61 @@
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+from aiogram.dispatcher import FSMContext
+import asyncio
+
+api = input('Введите token:')
+bot = Bot(token=api)
+dp = Dispatcher(bot, storage=MemoryStorage())
+keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
+btn1 = KeyboardButton('Рассчитать')
+btn2 = KeyboardButton('Информация')
+keyboard.add(btn1)
+keyboard.add(btn2)
+
+
+class UserState(StatesGroup):
+    age = State()
+    growth = State()
+    weight = State()
+
+
+@dp.message_handler(commands=['start'])
+async def start(message):
+    await message.answer('Клавиатура по команде /start:++', reply_markup=keyboard)
+
+
+@dp.message_handler(text=['Рассчитать'])
+async def set_age(message):
+    await message.answer('Введите свой возраст:')
+    await UserState.age.set()
+
+
+@dp.message_handler(state=UserState.age)
+async def set_growth(message, state):
+    await state.update_data(age=message.text)
+    await message.answer('Введите свой рост:')
+    await UserState.growth.set()
+
+
+@dp.message_handler(state=UserState.growth)
+async def set_weight(message, state):
+    await state.update_data(growth=message.text)
+    await message.answer('Введите свой вес:')
+    await UserState.weight.set()
+
+
+@dp.message_handler(state=UserState.weight)
+async def send_calories(message, state):
+    await state.update_data(weight=message.text)
+    data = await state.get_data()
+    age = float(data['age'])
+    growth = float(data['growth'])
+    weight = float(data['weight'])
+    calories = 10 * weight + 6.25 * growth - 5 * age + 5
+    await message.answer(f'Калории: {calories}')
+    await state.finish()
+
+
+executor.start_polling(dp, skip_updates=True)
